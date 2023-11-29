d217e5b523dbfa63901fcf3a8be049b4037707e6
Jack Boylan <boylan.jack@gmail.com>
2023-11-29 15:22:10 -0500

    Removes eval batch > 1; it's too complex

--- detectron2/config/defaults.py
+++ detectron2/config/defaults.py
@@ -620,7 +620,6 @@ _C.SOLVER.CHECKPOINT_PERIOD = 5000
 # and IMS_PER_BATCH = 32, each GPU will see 2 images per batch.
 # May be adjusted automatically if REFERENCE_WORLD_SIZE is set.
 _C.SOLVER.IMS_PER_BATCH = 16
-_C.SOLVER.IMS_PER_EVAL_BATCH = 16
 
 # The reference number of workers (GPUs) this config is meant to train with.
 # It takes no effect when set to 0.
--- tools/train_net.py
+++ tools/train_net.py
@@ -181,18 +181,6 @@ class Trainer(DefaultTrainer):
         return model
 
 
-    @classmethod
-    def build_test_loader(cls, cfg, dataset_name):
-        """
-        Returns:
-            iterable
-
-        It now calls :func:`detectron2.data.build_detection_test_loader`.
-        Overwrite it if you'd like a different data loader.
-        """
-        return build_detection_test_loader(cfg, dataset_name, clip_batch_size=cfg.SOLVER.IMS_PER_EVAL_BATCH)
-
-
 def setup(args):
     """
     Create configs and perform basic setups.
